b1834b9292da007a69e60fb218703b47d5e34d43
joshharris1 <josh.harris@cgi.com>
2018-11-12 10:58:40 +0000

    updated, no tests yet

--- src/banktechtest.js
+++ src/banktechtest.js
@@ -12,32 +12,32 @@ function Account() {
   this.INITIAL_BALANCE = 100;
   this.MINIMUM_BALANCE = 0;
   this.balance = 0;
-}
+};
 
 Account.prototype.setInitialBalance = function() {
   this.balance += this.INITIAL_BALANCE;
-}
+};
 
 Account.prototype.getBalance = function() {
   return.thisBalance;
-}
+};
 
 function Deposit(amount) {
   this.balance += amount;
-}
+};
 
 Bank.prototype.Deposit = function() {
   return.
-}
+};
 
 function Withdraw(amount) {
   this.balance -= amount;
-}
+};
 
-Withdraw.prototype. = function () {
+Withdraw.prototype.withdraw = function () {
 
 };
 
 function Transaction() {
-  this.
-}
+  
+};
